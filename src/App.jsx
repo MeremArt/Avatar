447798ab13cb851a0323045d5b7b2ec65db46e9c
Ugo Chinememerem <ugofranklin22@gmail.com>
2023-12-21 09:33:01 +0100

    comeandgo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+import SupportEngine from "./components/SupportEngine/SupportEngine";
+function App() {
+  return (
+    <>
+      <SupportEngine />
+    </>
+  );
+}
+
+export default App;
